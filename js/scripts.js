e08638d1398045039cb39d362bd6903e62ae2440
jacob ruleaux and Michelle Poterek <jacob@>
2017-06-06 15:49:46 -0700

    make the js operational as a weapon of mass luckstruction

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("form#fortune").submit(function(event) {
+    event.preventDefault();
+
+    var userResponseB = [];
+    $("input:checkbox[name=bad-luck]:checked").each(function(){
+      var badLuck = $(this).val();
+      userResponseB.push(badLuck);
+      console.log(userResponseB);
+    });
+    var userResponseG = [];
+    $("input:checkbox[name=good-luck]:checked").each(function(){
+      var goodLuck = $(this).val();
+      userResponseG.push(goodLuck);
+      console.log(userResponseG);
+    });
+    if (userResponseB.length >= userResponseG.length) {
+      $("#sucky").show();
+    } else {
+      $("#lucky").show();
+    }
+});
+});
